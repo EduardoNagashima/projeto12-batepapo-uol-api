1c32883954cfeb0dcbdf29539b5d6c7abfb48ba8
Eduardo Nagashima <eduyudji@gmail.com>
2022-04-30 16:41:52 -0300

    post /participants implemented

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
 import express from "express";
+import cors from "cors";
+import {MongoClient} from "mongodb";
+import joi from "joi";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.get('/teste', (req,res)=>{
+const participantSchema = joi.object({
+    name: joi.string().required()
+});
 
+let db;
+const mongoClient = new MongoClient("mongodb://127.0.0.1:27017");
+mongoClient.connect().then(()=>{
+    db = mongoClient.db("projeto12")
+})
 
-    res.send("Está funcionando!");
+app.post('/participants', (req,res)=>{
+    const {name} = req.body;
+    
+    const validation = participantSchema.validate(req.body);
+
+    if (validation.error){
+        console.log(validation.error.details);
+        res.status(422).send('Erro ao cadastrar');
+        return;
+    }
+
+    db.collection("participants").insertOne({"name": name});
+
+    res.sendStatus(201);
 })
 
 app.listen(5000);
